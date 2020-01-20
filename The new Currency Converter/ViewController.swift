80b1fbfc3f32a137b5d44c51cad9c4e0f52a463b
Mike Dix <godflowdix@gmail.com>
2020-01-20 00:18:20 -0500

    Initial Commit

---
+++ The new Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The new Currency Converter
+//
+//  Created by Mike Dix on 1/20/20.
+//  Copyright © 2020 Mike Dix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
